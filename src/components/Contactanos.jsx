fa66ff1f0222314dd92e2daab66054dcaebd7d29
monisz <79484792+monisz@users.noreply.github.com>
2021-11-13 00:52:15 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/components/Contactanos.jsx
@@ -0,0 +1,70 @@
+import React from "react";
+import { Form, Container, Button } from "react-bootstrap";
+
+const Contactanos = () => {
+  return (
+    <div className="encabezado">
+      <div className="header-general">
+        <img src="./img/contact.png" className="img-fluid" alt="uso celular" />
+        <div className="header-fTitulo">
+          <h1 className="header-titulo">Contactanos</h1>
+        </div>
+      </div>
+
+      <div className="container">
+        <div className="textServices1">
+          <h3>¡Comunicate con nosotros!</h3>
+        </div>
+
+        <p>
+          Completando los siguientes datos podremos avanzar para potenciar tu
+          proyecto
+        </p>
+      </div>
+
+      <Container
+        action="mailto:ojmanias1985@gmail.com"
+        method="post"
+        enctype="text/plain"
+      >
+        <Form>
+          <div className="container-form">
+            <div className="row">
+              <div class="col-sm-6">
+                <Form.Group className="mb-3">
+                  <Form.Label>Nombre</Form.Label>
+                  <Form.Control type="text" placeholder="" />
+                </Form.Group>
+              </div>
+
+              <div class="col-sm-6">
+                <Form.Group className="mb-3">
+                  <Form.Label>Correo Electrónico</Form.Label>
+                  <Form.Control type="email" placeholder="" />
+                </Form.Group>
+              </div>
+            </div>
+          </div>
+
+          <div className="container-form">
+            <Form.Group className="mb-3">
+              <Form.Label>Asunto</Form.Label>
+              <Form.Control type="text" placeholder="" />
+            </Form.Group>
+            <Form.Group className="mb-3">
+              <Form.Label>Tu consulta</Form.Label>
+              <Form.Control as="textarea" rows={3} />
+            </Form.Group>
+          </div>
+        </Form>
+        <div className="btnSend">
+          <Button>
+            <a href="mailto:pruebaFede@gmail.com">Enviar</a>
+          </Button>{" "}
+        </div>
+      </Container>
+    </div>
+  );
+};
+
+export default Contactanos;
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { Navbar, Nav } from "react-bootstrap";
+
+const NavBar = () => {
+  return (
+    <div>
+      <Navbar collapseOnSelect expand="lg" sticky="top" className="nav-general">
+        <Navbar.Brand href="/">
+          <img className="nav-logo" src="/img/logo.png" alt="logo dive4learn" />
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto" id="nav-menu">
+            <Nav.Link id="nav-item" href="/" title="Home">
+              HOME
+            </Nav.Link>
+
+            <Nav.Link id="nav-item" href="/servicios" title="Servicios">
+              SERVICIOS
+            </Nav.Link>
+
+            <Nav.Link
+              id="nav-item"
+              href="/elegirnos"
+              title="¿Por qué elegirnos?"
+            >
+              ¿POR QUÉ ELEGIRNOS?
+            </Nav.Link>
+            <Nav.Link id="nav-item" href="/contactanos" title="Contactanos">
+              CONTACTANOS
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+    </div>
+  );
+};
+
+export default NavBar;
